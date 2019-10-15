a4b80e95a3d07b8d5b72b94b7aa2314ffeb093f4
cDc <cdc.seacave@gmail.com>
2019-10-15 01:03:40 -0700

    original version

---
+++ getPaths.m
@@ -0,0 +1,6 @@
+function [dataPath,resultsPath]=getPaths()
+
+% make sure the paths point to the data and where you want the results to be stored
+
+dataPath='../MVS Data/';
+resultsPath=[dataPath '/Results/'];
